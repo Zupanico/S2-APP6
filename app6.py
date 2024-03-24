bd9d91f00d6c36d02698c0aa46f71cd33c0a0a2f
Zupanico <50524719+Zupanico@users.noreply.github.com>
2024-03-24 15:23:22 -0400

    first commit

---
+++ app6.py
@@ -0,0 +1,24 @@
+"""
+Fichier: app6.py
+But: simuler les filtres et imprimer leurs graphiques
+Date: 24/03/2024
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as signal
+
+import helpers as hp
+
+
+def passebas1():
+    print("passebas1")
+
+
+def main():
+    print("Début du programme")
+    passebas1()
+
+
+if __name__ == '__main__':
+    main()
